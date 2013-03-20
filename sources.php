da81c52a0c8c8de4c44418d3133e9b5ad3ca3e2c
Thomas <t.tetia@rt-iut.re>
2013-03-20 21:09:23 +0400

    Create sources.php

---
+++ sources.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<?php include ('head.php');?>
+  </head>
+
+  <body>
+<?php include('nav.php');?>
+
+    <div class="container">
+      <div class="hero-unit">
+           <div class="pull-right"><h2small>Visualiser vous intéresse?</h2small></div>
+          Télécharger notre agent, et bénéficier vous aussi de cet outil.
+          
+      </div> <!--hero-unit-->
+
+  </div> <!--Div container-->
+
+  </body>
+
+    <?php include('footer.php');?>
+    
+</html>
